d63da6a9fce69f4fa69f6283ec6edd4c6dea110f
qm210 <quantenmace@gmail.com>
2020-12-25 03:20:12 +0100

    and a nice move cursor

--- src/components/SceneShaderView.js
+++ src/components/SceneShaderView.js
@@ -96,6 +96,9 @@ const SceneShaderView = ()  => {
                         uniforms = {{
                             time: {type: '1f', value: time}
                         }}
+                        style = {{
+                            cursor: 'move',
+                        }}
                     />
                 }
             </ShaderFrame>
